79e45341d6862aa293a384e451bc693047cffa91
Vincentius Arnold <vincentiusarnoldfridolin@gmail.com>
2024-01-14 23:35:32 +0700

    Test Webhook

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const shell = require('shelljs');
 const app = express();
 
 app.get('/', async (req, res) => {
+
     // Notes : chmod +x [dir]
     const dir = "[fill]/script.sh";
     shell.exec(dir);
     return;
+
 });
 
 app.listen(3001);
